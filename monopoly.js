d72707ec41032613fc9da6f1928f2128daedb7d6
jotitan <titanbar@hotmail.com>
2013-08-30 07:46:11 +0200

    Refactoring et integration bloc hypo

--- monopoly.js
+++ monopoly.js
@@ -1883,6 +1883,7 @@ Object.defineProperty(Array.prototype, "size", {
       initFiches();
       initPlateau(plateau,initJoueurs);
       initDes();
+	 GestionTerrains.init();
       
      
   }
@@ -2041,6 +2042,296 @@ Object.defineProperty(Array.prototype, "size", {
       $('#idDetailFiche').slideUp();
   }
 
+
+
+  
+  var GestionTerrains = {
+	  maisonsToLever:[],
+	  changesConstructions:[],
+	  cout:0,
+	  totalRestant:0,
+	  divCout:null,
+	  divArgentRestant:null,
+	  /* Remet a 0 le panneau */
+	  reset:function() {
+	    this.Hypotheque.reset();
+	    this.LeverHypotheque.reset();
+	    this.Constructions.reset();
+	    this.cout=0;
+	    $('#coutAchats > span[name]').text(0);
+	    $('.currency-value').text(CURRENCY);
+	    this.update();		  
+	  },
+	  init:function(){
+		this.divArgentRestant = $('#idArgentRestant');
+		this.divCout = $('#idCoutTotal');
+		this.Hypotheque.init();
+		this.LeverHypotheque.init();
+		this.Constructions.init();
+		$('#housesPanel').dialog({
+		  width:800,
+		  height:600,
+		  title:'Gestion des maisons',
+		  modal:true,
+		  buttons:{'Fermer':function(){$('#housesPanel').dialog('close');},"Valider":function(){GestionTerrains.valider();}},
+		  autoOpen:false,
+		  open:function(){
+			  // On charge les proprietes a hypothequer
+			  GestionTerrains.load();
+		  }			
+		});
+	  },
+	  /* Charge les informations */
+	  load:function(){
+		  this.reset();
+		  this.Hypotheque.load();
+		  this.LeverHypotheque.load();
+		  this.Constructions.load();
+	  },
+	  /* Modifie le cout global */
+	  addCout:function(montant){
+		  this.cout+=montant;
+		  this.update();
+	  },
+	  /* Mets a jour le panneau */
+	  update:function(){
+	   // On mets a jour les maisons, si on ajoute, on ne peut pas hypothequer
+	    this.Hypotheque.update();			  
+	    var totals = this.Constructions.update();
+	    this.divCout.text((this.cout-totals.cout) + " " + CURRENCY);
+	    this.totalRestant = joueurCourant.montant + this.cout-totals.cout;
+	    this.divArgentRestant.text((this.totalRestant) + " " + CURRENCY);		  
+	  },
+	  verify:function(){
+		  try{
+			 if(GestionTerrains.totalRestant < 0){
+			   throw "Opération impossible : pas assez d'argent";
+			 }
+			  GestionTerrains.Constructions.verify();
+		  }catch(e){
+			  alert(e);
+			  return false;
+		  }
+		  return true;
+	  },
+	  valider:function(){
+		    if(!this.verify()){
+			 return;
+		    }
+		    this.Hypotheque.valider();
+		    this.LeverHypotheque.valider();
+		    this.Constructions.valider();
+		    
+		    this.closePanel();
+	  },
+	  closePanel:function(){
+		$('#housesPanel').dialog('close');
+	  },
+	  /* Gere l'hypotheque de terrain */
+	  Hypotheque:{
+		  table:[],
+		  select:null,
+		  div:null,
+		  init:function(){
+		    this.select = $('select','#idTerrains');
+		    this.div = $('#toHypotheque');
+		  },
+		  reset:function(){
+			  this.table=[];
+			  this.select.empty();
+			  this.div.empty();
+			  $('#idHypothequeAction').unbind('click').bind('click',function(){GestionTerrains.Hypotheque.add();});
+		  },
+		  valider:function(){
+		    // On recupere les fiches et on hypotheque les biens
+		    for(var id in this.table){
+			  this.table[id].hypotheque();
+		    }
+		  },				
+		  update:function(){
+			  $('option[data-color]',this.select).removeAttr('disabled');
+			  var colors = GestionTerrains.Constructions.getGroupesConstruits();
+			  for (var i in colors){
+				  $('option[data-color="' + colors[i] +'"]',this.select).attr('disabled','disabled');
+			  }
+		  },			
+		  /* Charge les terrains hypothequables */
+		  load:function(){		    
+			  var proprietes = joueurCourant.findMaisonsHypothecables();
+			    $(proprietes).each(function(){
+				    GestionTerrains.Hypotheque.addOption(this);
+			    });
+		  },
+		  addOption:function(fiche){
+			var option = $("<option data-color='" + fiche.color + "' value='" + fiche.id + "'>" + fiche.nom + " (+" + fiche.montantHypotheque + " " + CURRENCY + ")</option>");
+			option.data("fiche",fiche);			
+			this.select.append(option);
+		  },
+		  /* Ajoute une propriete aux hypotheques */
+		  add:function(){
+			  var terrain =  $('option:selected',this.select);
+			  var fiche = terrain.data("fiche");
+			  this.table[fiche.id] = fiche;
+			  var div = $('<div>' + fiche.nom + '</div>');
+			  var boutonAnnuler = $('<button style="margin-right:5px">Annuler</button>');
+			  var _self = this;
+			  boutonAnnuler.click(function(){
+				  _self.addOption(fiche);
+				  $(this).parent().remove();
+				  GestionTerrains.addCout(-fiche.hypotheque);
+				  delete _self.table[fiche.id];
+			  });
+			  div.prepend(boutonAnnuler);
+			  this.div.append(div)
+			  $('option:selected',this.select).remove();
+			  GestionTerrains.addCout(fiche.montantHypotheque);
+		  }
+	  },
+	  LeverHypotheque:{
+		  div:null,
+		  table:[],
+		  init:function(){
+		    this.div = $('#idTerrainsHypotheques > div');
+		  },
+		  reset:function(){
+			  this.div.empty();
+			  this.table=[];
+		  },
+		  valider:function(){
+		    for(var id in this.table){
+			  this.table[id].leveHypotheque();
+		    }
+		  },
+		  load:function(){
+			  var proprietes = joueurCourant.findMaisonsHypothequees();
+		    $(proprietes).each(function(){
+			   var fiche = this;
+			   var div = $("<div>" + this.nom + "</div>");
+			   var boutonLever = $('<button style="margin-right:5px">Lever</button>');
+			   boutonLever.click(function(){
+				  GestionTerrains.LeverHypotheque.lever($(this),fiche);
+			   });
+			   div.prepend(boutonLever);
+			   GestionTerrains.Hypotheque.append(div);
+		    });
+		  },
+		  lever:function(input,fiche){
+			  input.attr('disabled','disabled');
+			  this.table.push(fiche);
+			  GestionTerrains.addCout(-Math.round(fiche.montantHypotheque*1.1));
+		  },
+		  
+	  },
+	  Constructions:{
+		  table:[],
+		  div:null,
+		  infos:null,
+		  init:function(){
+		    this.div = $('#idTerrainsConstructibles');
+		    this.infos = $('#coutAchats');
+		  },
+		  /* Verifie pour la validation et renvoie une exception */
+		  verify:function(){
+			  var testGroups = [];
+			  $('select[data-color]',this.div).each(function(){
+				  var color = $(this).get(0).dataset.color;
+				  if (testGroups[color]==null) {
+					  testGroups[color] = {min:5,max:0};
+				  }
+				  testGroups[color].min = Math.min(testGroups[color].min,$(this).val());
+				  testGroups[color].max = Math.max(testGroups[color].max,$(this).val());
+			  });
+			  for (var color in testGroups) {
+				  if(testGroups[color].max - testGroups[color].min > 1){
+				    throw "Il faut équilibrer les maisons";  
+				  }
+			 }
+		  },
+		  valider:function(){
+		    for (var achat in this.table) {
+			  var data = this.table[achat];
+			  data.propriete.setNbMaison(data.nbMaison);
+			  joueurCourant.payer(data.cout);
+		    }
+		  },
+		  reset:function(){
+			  this.table = [];
+			  this.div.empty();
+		  },
+		  getGroupesConstruits:function(){
+			  var colors = [];
+			  $('select[data-color][value!=0]',this.div).each(function(){
+				  colors.push($(this).attr('data-color'));
+			  });
+			  return colors;
+		  },
+		  update:function(){
+			  var totals = {nbMaison:0,nbHotel:0,cout:0}
+			  for (var achat in this.table) {
+				  var data = this.table[achat];
+				  totals.cout+=data.cout;
+				  totals.nbMaison+=data.maison || 0;
+				  totals.nbHotel+=data.hotel || 0;
+			  }
+			  $('span[name]',this.infos).each(function(){
+				  $(this).text(totals[$(this).attr('name')]);
+			  });
+			  return totals;
+		  },
+		  load:function(){
+			  var groups = joueurCourant.findGroupes();
+			  var table = $('#idTerrainsConstructibles');
+			  for(var color in groups) {
+				  var divTitre = $('<div style="cursor:pointer">Groupe <span style="color:' + color + ';font-weight:bold">' + groups[color].proprietes[0].groupe + '</span></div>');
+				  divTitre.data("color",color.substring(1));
+				  divTitre.click(function(){
+					  var id = 'div.propriete-' + $(this).data('color');
+					  if($(id + ':visible',this.div).length == 0){
+						  $(id,this.div).slideDown();	// On ouvre
+					  }
+					  else{
+						  $(id,this.div).slideUp();
+					  }
+				  });			  
+				  this.div.append(divTitre);
+				  for(var index in groups[color].proprietes){
+					  var propriete = groups[color].proprietes[index];
+					  var divTerrain = $('<div class="propriete propriete-' + propriete.color.substring(1) + '"></div>');
+					  divTerrain.append('<span style="color:' + propriete.color + '" class="title-propriete">' + propriete.nom + '</span>');
+					  var select = $('<select data-color="' + propriete.color + '" class="' + ((propriete.nbMaison==5)?'hotel':'maison') + '"></select>');
+					  select.data("propriete",propriete);
+					  for (var j = 0; j <= ((false) ? propriete.nbMaison : 5); j++) {
+						  select.append("<option class=\"" + ((j==5)?"hotel":"maison") + "\" value=\"" + j + "\" " + ((propriete.nbMaison == j) ? "selected" : "") + ">x " + ((j==5)?1:j) + "</option>");
+					  }
+					  var _self = this;
+					  select.change(function(){
+						  var prop = $(this).data("propriete");
+						  // On verifie changement par rapport a l'origine
+						  if (prop.nbMaison == $(this).val()) {
+							  delete _self.table[prop.id];
+							  $('~span',this).text("");
+							  GestionTerrains.update();
+							  return;
+						  }
+						  var data = ($(this).val() == 5)?{hotel:1}:{maison:$(this).val() - prop.nbMaison};
+						  data.propriete = prop;
+						  data.nbMaison = $(this).val();
+						  data.cout = ($(this).val() > prop.nbMaison)
+						    ?($(this).val()-prop.nbMaison)*prop.prixMaison
+						    :($(this).val()-prop.nbMaison )*prop.prixMaison/2;
+						    $(this).removeClass().addClass(($(this).val()==5)?'hotel':'maison');
+						  $('~span',this).text(data.cout);
+						  _self.table[prop.id] = data;
+						  GestionTerrains.update();
+				   });
+				   divTerrain.append(select).append('<span></span> ' + CURRENCY);						 
+				   $(this.div).append(divTerrain);
+				  }
+			  }
+		  }		
+	  }	  
+  };
+  
 
   function loadFiche(fiche) {
       loadGenericFiche(fiche, $('#fiche'), 'FFFFFF');

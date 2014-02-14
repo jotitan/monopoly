ddf338143cad39b76e766c69c3b42695f05e74df
jotitan <titanbar@hotmail.com>
2014-02-14 09:17:43 +0100

    Ajout configuration ftp

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,14 +15,27 @@ module.exports = function(grunt){
 		zip:{
 			'build.zip':['build/**']
 			
+		},
+		ftp-deploy:{
+			build:{
+				auth:{
+					host:'ftpperso.free.fr',
+					port:21,
+					// Definir les droits dans le fichier .ftppass
+					authKey:'default'
+				},
+				src:'build',
+				desc:'monopoly/'
+			}
 		}
-
 	})
 
 	grunt.loadNpmTasks('grunt-contrib-clean')
 	grunt.loadNpmTasks('grunt-contrib-copy')
 	grunt.loadNpmTasks('grunt-zip')
+	grunt.loadNpmTasks('grunt-ftp-deploy')
 
-	grunt.registerTask('package','package application',['clean','copy','zip'])	
-	//grunt.registerTask('clean','nettoie les repertoires',['clean'])	
+	grunt.registerTask('package','package application',['clean','copy','zip'])
+	grunt.registerTask('deploy','deploie application sur serveur ftp',['ftp-deploy'])	
+	grunt.registerTask('clean','nettoie les repertoires',['clean'])	
 }

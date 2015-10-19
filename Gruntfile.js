eaf879edf9d65e895c39707e6296221c07b81584
jotitan <titanbar@hotmail.com>
2015-10-19 15:31:42 +0200

    Correct copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt){
   			}
   		},
   		copy: {
-			target:{
+			target:{expand:true,
 				src:['lib/**','css/**','img/**','data/**','favicon.ico'],
 				dest:'build/'
 			}
